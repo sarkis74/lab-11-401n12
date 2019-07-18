288af6d2fa1299291def9b4a224455dc111a73b7
Sarkis <sarkis_aghazarian@yahoo.com>
2019-07-18 15:05:34 -0700

    pushing latest

--- __tests__/middleware.test.js
+++ __tests__/middleware.test.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const supergoose = require('./supergoose');
-const auth = require('../src/auth/middleware');
-const Users = require('../src/auth/users-model.js');
-
+const supergoose = require('../../supergoose.js');
+const auth = require('../../../src/auth/middleware.js');
+const Users = require('../../../src/auth/users-model.js');
 
 let users = {
     admin: {username: 'admin', password: 'password', role: 'admin'},
@@ -21,7 +20,7 @@ beforeAll(async (done) => {
 
 afterAll(supergoose.stopDB);
 
-describe('auth middleware', () => {
+describe('Auth Middleware', () => {
 
     // admin:password: YWRtaW46cGFzc3dvcmQ=
     // admin:foo: YWRtaW46Zm9v
@@ -30,6 +29,8 @@ describe('auth middleware', () => {
 
     describe('user authentication', () => {
 
+        let cachedToken;
+
         it('fails a login for a user (admin) with the incorrect basic credentials', () => {
 
             let req = {
@@ -37,14 +38,7 @@ describe('auth middleware', () => {
                     authorization: 'Basic YWRtaW46Zm9v',
                 },
             };
-            let res = {};
-            let next = jest.fn();
-            let middleware = auth;
 
-            return middleware(req, res, next)
-                .then(() => {
-                    expect(next).toHaveBeenCalledWith(errorObject);
-                });
 
         });
 
@@ -55,38 +49,10 @@ describe('auth middleware', () => {
                     authorization: 'Basic YWRtaW46cGFzc3dvcmQ=',
                 },
             };
-            let res = {};
-            let next = jest.fn();
-            let middleware = auth;
 
-            return middleware(req, res, next)
-                .then(() => {
-                    let savedToken = req.token;
-                    expect(next).toHaveBeenCalledWith();
-                });
 
-        });
-        it('gives error when unauthorized user tries to GET /books', () => {
-            return mockRequest
-                .get('/books')
-                .then(results => {
-                    console.log(results.text)
-                    expect(results.status).toEqual(401);
-                    expect(results.text).toEqual('{"error":"Invalid User ID/Password"}');
-                });
-        });
-
-        it('returns books when an authorized user tries to GET /books', () => {
-            return mockRequest
-                .get('/books')
-                .set('Authorization', 'Basic YWRtaW46cGFzc3dvcmQ=')
-                .then(results => {
-                    console.log(results.body)
-                    expect(results.status).toEqual(200);
-                    expect(results.body.count).toEqual(3);
-                    expect(results.body.results[0].title).toEqual('Moby Dick');
-                });
         });
 
     });
+
 });
